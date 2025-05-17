5bea58e393c2e0dafab192bf5e3e19d80e2089bb
ChoiMari <chl98639863@gmail.com>
2025-05-18 01:14:14 +0900

    실습 문제 풀이 추가

--- WindowsFormsApp_02_Variable/WindowsFormsApp_02_Variable/Form1.Designer.cs
+++ WindowsFormsApp_02_Variable/WindowsFormsApp_02_Variable/Form1.Designer.cs
@@ -37,57 +37,61 @@
             // 
             // textBox_print
             // 
-            this.textBox_print.Location = new System.Drawing.Point(174, 118);
+            this.textBox_print.Location = new System.Drawing.Point(152, 94);
+            this.textBox_print.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_print.Multiline = true;
             this.textBox_print.Name = "textBox_print";
-            this.textBox_print.Size = new System.Drawing.Size(524, 305);
+            this.textBox_print.Size = new System.Drawing.Size(459, 245);
             this.textBox_print.TabIndex = 0;
-            this.textBox_print.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Gulim", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(171, 83);
+            this.label1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(150, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 28);
+            this.label1.Size = new System.Drawing.Size(86, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Result : ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 118);
+            this.textBox1.Location = new System.Drawing.Point(30, 94);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 59);
+            this.textBox1.Size = new System.Drawing.Size(88, 48);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(34, 196);
+            this.textBox2.Location = new System.Drawing.Point(30, 157);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 60);
+            this.textBox2.Size = new System.Drawing.Size(88, 49);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(193, 12);
+            this.textBox3.Location = new System.Drawing.Point(169, 10);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(444, 50);
+            this.textBox3.Size = new System.Drawing.Size(389, 41);
             this.textBox3.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 360);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox_print);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
