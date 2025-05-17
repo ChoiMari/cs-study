9321a09a6caec521671e091d0825b5f51a6a3357
ChoiMari <chl98639863@gmail.com>
2025-05-18 02:33:49 +0900

    사용자 입력 받기, switch문, enum, goto(점프문)

---
+++ WindowsFormsApp_07_Condition/WindowsFormsApp_07_Condition/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp_07_Condition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.radioButton_true = new System.Windows.Forms.RadioButton();
+            this.radioButton_false = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(60, 45);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.Size = new System.Drawing.Size(630, 85);
+            this.textBox_result.TabIndex = 0;
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(60, 161);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(630, 85);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(60, 285);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(630, 85);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // radioButton_true
+            // 
+            this.radioButton_true.AutoSize = true;
+            this.radioButton_true.Location = new System.Drawing.Point(75, 397);
+            this.radioButton_true.Name = "radioButton_true";
+            this.radioButton_true.Size = new System.Drawing.Size(92, 16);
+            this.radioButton_true.TabIndex = 1;
+            this.radioButton_true.TabStop = true;
+            this.radioButton_true.Text = "radioButton1";
+            this.radioButton_true.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_false
+            // 
+            this.radioButton_false.AutoSize = true;
+            this.radioButton_false.Location = new System.Drawing.Point(196, 397);
+            this.radioButton_false.Name = "radioButton_false";
+            this.radioButton_false.Size = new System.Drawing.Size(92, 16);
+            this.radioButton_false.TabIndex = 1;
+            this.radioButton_false.TabStop = true;
+            this.radioButton_false.Text = "radioButton1";
+            this.radioButton_false.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton_false);
+            this.Controls.Add(this.radioButton_true);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.textBox_print);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.RadioButton radioButton_true;
+        private System.Windows.Forms.RadioButton radioButton_false;
+    }
+}
+
