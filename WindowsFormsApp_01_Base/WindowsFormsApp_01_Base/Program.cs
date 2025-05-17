e38428a54e151df06e2f1a06e218fb0576172529
ChoiMari <chl98639863@gmail.com>
2025-05-18 00:06:31 +0900

    윈도우폼 기본 구조

---
+++ WindowsFormsApp_01_Base/WindowsFormsApp_01_Base/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_01_Base
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // Form1이라는 윈도우폼을 실행
+            //new Form1()을 통해 폼 클래스의 인스턴스가 생성되고, 실행됨
+        }
+    }
+}
