a504ed572e7fa8692daff77322b8fcfcadcc7471
ChoiMari <chl98639863@gmail.com>
2025-05-18 01:16:08 +0900

    재그드 배열 실습 풀이 추가

--- WindowsFormsApp_03_Array/WindowsFormsApp_03_Array/Form1.cs
+++ WindowsFormsApp_03_Array/WindowsFormsApp_03_Array/Form1.cs
@@ -177,36 +177,21 @@ namespace WindowsFormsApp_03_Array
     }
 }
 
-/* 실습 3번 문제 풀이)
+/* 
  
- // 실습3번) 종합문제
-string productName = "노트북";
-int price = 1200000;
-float discountRate = 0.15f;
-byte stock = 8;
-bool isAvailable = stock > 0;
+// 재그드 배열 실습 풀이
+            string[][] school_class = new string[3][];
 
-// 할인된 가격
-double discountedPrice = price * (1 - discountRate);
+            // 각각 직접 할당
+            school_class[0] = new string[2] { "철희", "미애" };
+            school_class[1] = new string[3] { "철수", "영미", "민수" };
+            school_class[2] = new string[1] { "수빈" };
 
-textBox1.Text = isAvailable ? "구매 가능: 할인 가격은 " +
-    discountedPrice.ToString("N0") + "원입니다." : "품절되었습니다.";
-
-textBox2.Text = (stock >= 5) ? "여유 있음" : "소량 남음";
-
-textBox3.Text = "상품명: " + productName
-    + ", 할인된 가격: " + discountedPrice.ToString("N0") + "원"
-    + ", 재고: " + stock.ToString() + "개";
-
-// # 소수점 포맷 설정
-// - 포맷 조정은 "출력용" 변환이다. (계산 자체가 바뀌진 X)
-// "N0"     - 천 단위 구분 쉼표(,) 찍어주는 서식.
-// "P1'     - 퍼센트로 변환해서 소수 첫째 자리까지.
-// "0"      - 자리 수 채움 (0으로 채움)
-// "0.0"    - 소수점 첫째 자리까지, 없으면 0 채움
-// "0.00"   - 소수점 둘째 자리까지, 없으면 0 채움
-// "0.###"  - 소수점 세 자리까지, 있으면 표시, 없으면 표시 안함.
-// "N2"     - 쉼표 + 소수 둘째자리까지.
+            // TextBox에 학생 출력
+            textBox2.Text = "";
+            textBox2.Text += "1반 학생 목록: \r\n";
+            textBox2.Text += "- " + school_class[0][0] + "\r\n"; 
+            textBox2.Text += "- " + school_class[0][1] + "\r\n"; 
  
  
  */
